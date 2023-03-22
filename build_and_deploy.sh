bd38eae4fc8687e64a33e4176ad4d6111740996c
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-03-22 16:03:09 +0900

    fixed path

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # assuming you have already cloned iris-oauth2
-cp  $HOME/git/iris-oauth2-private/client/environment.prod.ts src/environments/
-cp  $HOME/git/iris-oauth2-private/client/environment.ts src/environments/
+cp  $HOME/git/iris-oauth2/client/environment.prod.ts src/environments/
+cp  $HOME/git/iris-oauth2/client/environment.ts src/environments/
 ng build --configuration production --base-href=/myapp/
 echo "deploying to iris-oauth2"
-rm -fR $HOME/git/iris-oauth2-private/htdocs/myapp
-cp -fR dist/myapp $HOME/git/iris-oauth2-private/htdocs/
+rm -fR $HOME/git/iris-oauth2/htdocs/myapp
+cp -fR dist/myapp $HOME/git/iris-oauth2/htdocs/
 echo "done"
