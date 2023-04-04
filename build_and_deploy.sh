c0444a1bb4a1f8487ace09dd08150dc446a88d4d
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-04-04 16:58:08 +0900

    use relative path

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# assuming you have already cloned iris-oauth2-private
-cp  $HOME/git/iris-oauth2-private/client/environment.prod*.ts src/environments/
-cp  $HOME/git/iris-oauth2-private/client/environment.ts src/environments/
+# assuming you have already cloned iris-oauth2
+cp  ../iris-oauth2/client/environment.prod*.ts src/environments/
+cp  ../iris-oauth2/client/environment.ts src/environments/
 ng build --configuration production --base-href=/myapp/
 echo "deploying to iris-oauth2"
-rm -fR $HOME/git/iris-oauth2-private/htdocs/myapp/*
-cp -fR dist/myapp/* $HOME/git/iris-oauth2-private/htdocs/myapp/
+rm -fR ../iris-oauth2/htdocs/myapp/*
+cp -fR dist/myapp/* ../iris-oauth2/htdocs/myapp/
 
 ng build --configuration production2 --base-href=/myapp2/
 echo "deploying to iris-oauth2"
-rm -fR $HOME/git/iris-oauth2-private/htdocs/myapp2/*
-cp -fR dist/myapp/* $HOME/git/iris-oauth2-private/htdocs/myapp2/
+rm -fR ../iris-oauth2/htdocs/myapp2/*
+cp -fR dist/myapp/* ../iris-oauth2/htdocs/myapp2/
 
 echo "done"
 echo "Go to https://webgw.localdomain/myapp/ or https://webgw.localdomain/myapp2/"
