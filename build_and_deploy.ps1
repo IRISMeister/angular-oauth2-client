0ba477ce41808f84a17dcefc4a82c1945bfe11c7
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-04-07 13:44:11 +0900

    add rm

--- build_and_deploy.ps1
+++ build_and_deploy.ps1
@@ -4,11 +4,13 @@ $env:gitroot="\\wsl$\Ubuntu-22.04/home/irismeister/git/iris-oauth2"
 # assuming you have already cloned iris-oauth2
 cp $env:gitroot/client/environment.prod*.ts src/environments/
 
+rm -r -fo dist/myapp/*
 ng build --configuration production --base-href=/myapp/
 echo "deploying to iris-oauth2"
 rm -r -fo $env:gitroot/htdocs/myapp/*
 cp -fo -r dist/myapp/* $env:gitroot/htdocs/myapp/
 
+rm -r -fo dist/myapp/*
 ng build --configuration production2 --base-href=/myapp2/
 echo "deploying to iris-oauth2"
 rm -r -fo $env:gitroot/htdocs/myapp2/*
--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -3,11 +3,13 @@
 # assuming you have already cloned iris-oauth2
 cp  ../iris-oauth2/client/environment.prod*.ts src/environments/
 
+rm -fR dist/myapp/*
 ng build --configuration production --base-href=/myapp/
 echo "deploying to iris-oauth2"
 rm -fR ../iris-oauth2/htdocs/myapp/*
 cp -fR dist/myapp/* ../iris-oauth2/htdocs/myapp/
 
+rm -fR dist/myapp/*
 ng build --configuration production2 --base-href=/myapp2/
 echo "deploying to iris-oauth2"
 rm -fR ../iris-oauth2/htdocs/myapp2/*
